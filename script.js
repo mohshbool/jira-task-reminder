71922a9b126f50afc0105f22c31e46b642a9491f
mohshbool <mohammad.alshbool2002@gmail.com>
2019-05-02 01:33:07 +0300

    added initialization script and onbeforeunload event handler

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-let issues = [];
+let issues = [], username = '', email = '';
 chrome.storage.local.get(['username', 'email'], function(result) {
-  const { username, email } = result;
+  const { username: usernameS, email: emailS } = result;
   document.querySelectorAll('#ghx-pool > div.ghx-swimlane > ul > li:nth-child(2)').forEach(function(e) { //per section
-    for (let card of e.children[0].children) { // per card
-      const assignee = card.children[3].children[1].children[0].children[0].alt.slice(10);
-      if (new RegExp(username, 'i').test(assignee)) {
-        issues.push({ assignee, email, issueKey, issueTitle });
-      };
-    }; 
-  }); 
+    if (e.children[0].children.length > 0) {
+      for (let card of e.children[0].children) { // per card
+        const infoIndex = card.children.length === 5 ? 2 : 3;
+        const assignee = card.children[infoIndex].children[1].children[0].children[0].alt.slice(10);
+        const issueKey = card.children[infoIndex].children[1].children[1].getAttribute('aria-label');
+        const issueTitle = card.children[0].getAttribute('title');
+        if (new RegExp(username, 'i').test(assignee)) {
+          issues.push({ assignee, email, issueKey, issueTitle });
+          username = usernameS;
+          email = emailS;
+        };
+      }; 
+    };
+  });
+});
+
+window.onbeforeunload =  async function(event) {
+  console.log(issues, username, email);
   let data = {
     username,
     email,
+    issues: []
   };
-  fetch('https://jira-task-reminder.glitch.me/sendmail', {
+  for (let { issueKey, issueTitle } of issues) {
+    data.issues.push({ issueKey, issueTitle });
+  }
+  await fetch('https://jira-task-reminder.glitch.me/sendmail', {
     headers: { "Content-Type": "application/json; charset=utf-8" },
     method: 'POST',
     body: JSON.stringify(data)
-  }).then(() => {}).catch(() => {});
-});
+  });
+  event.returnValue = "You have attempted to leave this page. Are you sure?";
+  return "You have attempted to leave this page. Are you sure?";
+};
