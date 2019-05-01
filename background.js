982cd7b32bdffbbe58115266cdeba90abf1d7fea
mohshbool <mohammad.alshbool2002@gmail.com>
2019-05-02 00:40:48 +0300

    simplified script excution

--- background.js
+++ background.js
@@ -1,21 +1,13 @@
 chrome.runtime.onInstalled.addListener(function() {
-  let tab = {};
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, currTab) {
-    console.log(changeInfo)
-    if (changeInfo.status !== 'complete') {
+    if (changeInfo.status === 'complete') {
       const { title, url } = currTab;  
       if (/agile board/i.test(title)
       && /https:\/\/[a-z-_]+\.atlassian\.net\/secure\/RapidBoard\.jspa\?rapidView=\d+&projectKey=\w+/i.test(url)) {
-        tab = currTab;
+        chrome.tabs.executeScript(null, {
+          file: 'script.js'
+        });
       }
     }
-  });
-  chrome.tabs.onRemoved.addListener(function() {
-    if (tab.url) {
-      chrome.tabs.executeScript(null, {
-        file: 'script.js'
-      });
-    }
-    delete tab;
   });
 });
